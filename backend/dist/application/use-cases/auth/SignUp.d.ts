f8a5e988e4ba36ce376de0ceb84b6afbc5dc6a42
DrMartinel <thanhhoangworking@gmail.com>
2026-04-14 16:47:06 +0700

    feat: add user progress routes and schemas to backend server

--- backend/dist/application/use-cases/auth/SignUp.d.ts
+++
@@ -1,8 +0,0 @@
-import { IAuthRepository } from '../../../domain/repositories/IAuthRepository';
-import { SignUpDTO, AuthResultDTO } from '../../dtos/AuthDTOs';
-export declare class SignUpUseCase {
-    private readonly authRepository;
-    constructor(authRepository: IAuthRepository);
-    execute(dto: SignUpDTO): Promise<AuthResultDTO>;
-}
-//# sourceMappingURL=SignUp.d.ts.map
--- backend/dist/domain/errors/AuthErrors.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class AuthenticationError extends Error {
-    constructor(message: string);
-}
-export declare class UserAlreadyExistsError extends Error {
-    constructor(message: string);
-}
-//# sourceMappingURL=AuthErrors.d.ts.map
