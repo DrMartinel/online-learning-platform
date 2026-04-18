1234195277bc816424f8e1aea752a1f4ee2eda67
DrMartinel <thanhhoangworking@gmail.com>
2026-04-18 22:04:28 +0700

    chore: remove obsolete application files and use cases for user, lesson, and course management

--- backend/dist/application/use-cases/user-progress/GetLessonProgress.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserProgressDTO } from '../../dtos/UserProgressDTOs';
-import { IUserProgressRepository } from '../../../domain/repositories/IUserProgressRepository';
-export declare class GetLessonProgressUseCase {
-    private readonly repository;
-    constructor(repository: IUserProgressRepository);
-    execute(userId: string, lessonId: string): Promise<UserProgressDTO | null>;
-}
-//# sourceMappingURL=GetLessonProgress.d.ts.map
--- backend/dist/application/use-cases/user-progress/UpdateProgress.d.ts
+++
@@ -1,8 +0,0 @@
-import { UpdateUserProgressDTO, UserProgressDTO } from '../../dtos/UserProgressDTOs';
-import { IUserProgressRepository } from '../../../domain/repositories/IUserProgressRepository';
-export declare class UpdateProgressUseCase {
-    private readonly repository;
-    constructor(repository: IUserProgressRepository);
-    execute(id: string, dto: UpdateUserProgressDTO): Promise<UserProgressDTO>;
-}
-//# sourceMappingURL=UpdateProgress.d.ts.map
